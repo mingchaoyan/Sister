bc195aa1a32e50afc3e3d966dcaa790e2396bac2
mingchaoyan <mingchaoyan@gmail.com>
2016-11-05 16:24:59 +0800

    update

---
+++ char.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+  int inta = 97;
+  int intb = 98;
+  printf("%d\n", intb-inta);
+  int intc = 3;
+  printf("%f\n", (float)intc);
+
+  char chara = 'a';
+  char charb = 'b';
+  printf("%d\n", charb-chara);
+  char charc = 3;
+  printf("%f\n", (float)charc);
+
+}
